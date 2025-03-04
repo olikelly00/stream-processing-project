34e317edfccee552978f8d30a5b287c1ba53a777
Oli Kelly <olikelly@MASTUDENT-Oli-Kelly-NJ9KFPW96H.local>
2025-03-04 10:59:43 +0000

    update

--- consumer.py
+++ consumer.py
@@ -92,17 +92,17 @@ print("df redacted")
 
 df = data_frame.selectExpr("to_json(struct(*)) AS value")
 
-
 print("writing query")
 query = df.writeStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "b-1.greencluster.jdc7ic.c3.kafka.eu-west-2.amazonaws.com:9098") \
-    .option("topic", "processed-events") \
+    .format("console") \
     .option("checkpointLocation", "/tmp/kafka-checkpoint-raw") \
     .start()
 print("query written")
 
 
+# .format("kafka") \
+#     .option("kafka.bootstrap.servers", "b-1.greencluster.jdc7ic.c3.kafka.eu-west-2.amazonaws.com:9098") \
+#     .option("topic", "processed-events") \
 
 query.awaitTermination()
 
