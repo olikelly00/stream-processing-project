16d90b10e42971468420d301c0fb54d6c96f2d50
Edith Cheler <edithcheler@mastudemb03q6l4.home>
2025-03-04 14:50:38 +0000

    fraud detection

--- fraud_detection.py
+++ fraud_detection.py
@@ -74,24 +74,24 @@ def reset_dict(dict):
 cleanup_thread = threading.Thread(target=reset_dict, daemon=True)
 cleanup_thread.start()
 
-def detect_fraud(event):
+def detect_fraud(value):
     print("Hello fraudster!!!")
-    if event["event_name"] == "add_to_cart":
+    if value["event_name"] == "add_to_cart":
         print("Add to cart event detected")
-        if event["user_id"] not in add_to_cart_tracker.keys():
-            add_to_cart_tracker[event["user_id"]] = [time.now()]
+        if value["user_id"] not in add_to_cart_tracker.keys():
+            add_to_cart_tracker[value["user_id"]] = [time.now()]
             print(add_to_cart_tracker)
         else:
-            add_to_cart_tracker[event["user_id"]].append(time.now())
+            add_to_cart_tracker[value["user_id"]].append(time.now())
             print("I got here ----- in the else")
                 #[timestamp for timestamp in add_to_cart_tracker[event["user_id"]] if timestamp >= (current_time - 5)]
 
-        if len(add_to_cart_tracker[event["user_id"]]) >= 5:
+        if len(add_to_cart_tracker[value["user_id"]]) >= 5:
             print("Fraud")
             #reset_dict(add_to_cart_tracker)
 
 
-detect_fraud(df)
+detect_fraud(df["value"])
 
 
 
