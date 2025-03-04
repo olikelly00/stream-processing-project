82ae1010128afa9dd18b1027a5db39cb86a55f94
Edith Cheler <edithcheler@mastudemb03q6l4.home>
2025-03-04 13:45:54 +0000

    fraud detection

--- fraud_detection.py
+++ fraud_detection.py
@@ -1,7 +1,65 @@
+from confluent_kafka import Consumer, KafkaException
+from aws_msk_iam_sasl_signer import MSKAuthTokenProvider
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.functions import col, from_json, expr, lit
+import socket
+import json
 
-from consumer import data_frame
+def oauth_cb(oauth_config):
+    auth_token, expiry_ms = MSKAuthTokenProvider.generate_auth_token("eu-west-2")
+    return auth_token, expiry_ms/1000
 
-event = {}
 
-print(data_frame)
+
+spark = SparkSession.builder \
+    .appName("FraudDetection") \
+    .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,software.amazon.msk:aws-msk-iam-auth:2.2.0') \
+    .getOrCreate()
+
+
+kafka_options = {
+    "kafka.bootstrap.servers": "b-2-public.greencluster.jdc7ic.c3.kafka.eu-west-2.amazonaws.com:9198",
+    "kafka.sasl.mechanism": "AWS_MSK_IAM",
+    "kafka.security.protocol": "SASL_SSL",
+    "kafka.sasl.jaas.config": """software.amazon.msk.auth.iam.IAMLoginModule required awsProfileName="";""",
+    "kafka.sasl.client.callback.handler.class": "software.amazon.msk.auth.iam.IAMClientCallbackHandler",
+    "startingOffsets": "latest",
+    "subscribe": "events"
+}
+
+df = spark.readStream.format("kafka").options(**kafka_options).load()
+df = df.withColumn('decoded_value', col('value').cast('string'))
+
+
+schema = StructType([
+    StructField("user_id", StringType(), True),
+    StructField("event_name" , StringType(), True),
+    StructField("page" , StringType(), True),
+    StructField("item_url" , StringType(), True),
+    StructField("order_email" , StringType(), True),
+])
+
+
+data_frame = df.withColumn(
+    'parsed_value',
+    from_json(col('decoded_value'), schema)
+).select(
+    col("parsed_value.user_id").alias("user_id"),
+    col("parsed_value.event_name").alias("event_name"),
+    col("parsed_value.page").alias("page"),
+    col("parsed_value.item_url").alias("item_url"),
+    col("parsed_value.order_email").alias("order_email")
+)
+
+df = data_frame.selectExpr("to_json(struct(*)) AS value")
+
+
+query = df.writeStream \
+    .format("console") \
+    .options(**kafka_options) \
+    .option("checkpointLocation", "/tmp/kafka-checkpoints") \
+    .start()
+
+
+query.awaitTermination()
