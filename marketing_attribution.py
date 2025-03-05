0dbc64843b4a98e7d4808b8e8d5cb5ac1f57bbe4
Oli Kelly <olikelly@MASTUDENT-Oli-Kelly-NJ9KFPW96H.local>
2025-03-05 11:10:57 +0000

    update

--- marketing_attribution.py
+++ marketing_attribution.py
@@ -77,6 +77,7 @@ def track_marketing_channel(batch_df, batch_id):
                 user_tracker[user_id] = channel if channel else 'organic'
         elif event_name == 'order_confirmed':
             channel = user_tracker.get(user_id)
+    print("USER TRACKER: ", user_tracker)
     return channel
 
        
